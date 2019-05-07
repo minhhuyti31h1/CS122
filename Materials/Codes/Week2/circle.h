fe3152893219adde9a03f2c8380fe54f4515df61
thangdnsf <thangdn.tlu@gmail.com>
2019-05-07 16:58:34 +0700

    update

---
+++ Materials/Codes/Week2/circle.h
@@ -0,0 +1,78 @@
+#ifndef CIRCEL_H
+#define CIRCEL_H
+#include "point.h"
+#include <math.h>
+class circle
+{
+private:
+    double r;
+    point I;
+
+    /* data */
+public:
+    circle();
+    circle(point I,double r);
+    circle(double x,double y, double r);
+    ~circle();
+    void setI(const point &I );
+    void setr(double r);
+    double getr()const;
+    point getI()const;
+    double getArea()const;
+    double getPerimeter()const;
+    int exam(const point &)const;
+};
+#endif
+    circle::circle(){
+        this->r=0;
+        this->I=point(0,0);
+    }
+    circle::circle(point tam,double r){
+        this->I=tam;
+        this->r=r;
+    }
+    circle::circle(double x,double y,double r)
+    {
+        this->I=point(x,y);
+        this->r=r;
+    }
+    void circle::setI(const point &I)
+    {
+        this -> I=I;
+    }
+    void circle::setr(double r)
+    {
+        this -> r=r;
+    }
+    double circle::getr()const
+    {
+        return this -> r;
+    }
+    point circle::getI()const
+    {
+        return this -> I;
+    }
+    double circle::getArea()const
+    {
+        return 3.14*r*r;
+    }
+    double circle::getPerimeter()const
+    {
+        return 2*3.14*r;
+    }
+    int circle::exam(const point & k)const
+    {
+        double temp=sqrt(pow(I.getX()-k.getX(),2)+pow(I.getY()-k.getY(),2));
+        if(temp < r)
+        {
+            return 0;
+        }
+        else if(temp==r)
+        {
+            return 1;
+        }
+        else
+        {
+            return 2;
+        }       
+    }
