79da2ec9f17cd61b0a4c55539d0ae7146e9deb8f
minhhuyti31h1 <50070480+minhhuyti31h1@users.noreply.github.com>
2019-05-23 21:23:30 +0700

    Update NhanVienHH.cpp

--- Materials/Codes/Week3-TH/NhanVienHH.cpp
+++ Materials/Codes/Week3-TH/NhanVienHH.cpp
@@ -4,7 +4,46 @@
 NhanVienHH::NhanVienHH()
 {
 }
+#include "NhanVienHH.h"
 
+
+NhanVienHH::NhanVienHH()
+{
+}
+NhanVienHH::NhanVienHH(string , string , string, double,double):id(id), fullname(name), taxcode(tax),tong(tong),thuong(thuong)
+{}
+	//setters
+void NhanVienHH::settongdoanhthu(double tong )
+{
+    this->tong=tong;
+}
+void NhanVienHH::setmucthuong(double thuong)
+{
+    this-> thuong=thuong;
+}
+	//getters
+
+
+double NhanVienHH::gettongdoanhthu()const
+{
+    return this->tong;
+}
+double NhanVienHH::getmucthuong()const
+    return this->thuong;
+double NhanVienHH::getSalary()const
+{
+    return this->Salary;
+}
+	//method
+double NhanVienHH::getSalary()const
+	{	
+    return this->tongdoanhthu * this->mucthuong / 100;
+  }
+
+
+NhanVienHH::~NhanVienHH()
+{
+}
 
 NhanVienHH::~NhanVienHH()
 {
