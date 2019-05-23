fb181ce546805a5d2627d7ae45064781ed26f2ad
minhhuyti31h1 <50070480+minhhuyti31h1@users.noreply.github.com>
2019-05-23 21:00:09 +0700

    Update NhanVienHH.h

--- Materials/Codes/Week3-TH/NhanVienHH.h
+++ Materials/Codes/Week3-TH/NhanVienHH.h
@@ -9,9 +9,13 @@ public:
 	NhanVienHH();
 	NhanVienHH(string, string, string, double,double);
 	//setters
-
+	void settongdoanhthu(double tong);
+	void setmucthuong(double thuong);
+	
 	//getters
-
+	double gettongdoanhthu()const;
+	double  getmucthuong()const;
+	double getSalary()const;
 
 	//method
 	double getSalary()const
