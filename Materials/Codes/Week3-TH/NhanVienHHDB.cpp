bc8837e58e91accf85ddd5baac10aee937bdda59
minhhuyti31h1 <50070480+minhhuyti31h1@users.noreply.github.com>
2019-05-23 21:35:46 +0700

    Update NhanVienHHDB.cpp

--- Materials/Codes/Week3-TH/NhanVienHHDB.cpp
+++ Materials/Codes/Week3-TH/NhanVienHHDB.cpp
@@ -2,8 +2,29 @@
 
 
 NhanVienHHDB::NhanVienHHDB()
+{}
+NhanVienHHDB::NhanVienHHDB(string, string, string,double, double, double):id(id), fullname(name), taxcode(tax),tong(tong),thuong(thuong),base(base)
+{}
+	//setter;
+void setbase_salary(double base)
 {
+    this-> base=base;
 }
+	//getter;
+double getbase_salary()const
+{
+  return this->base;
+}
+double getSalary()const
+{
+    return this->Salary;
+}
+
+	//methods
+double getSalary()const
+	{
+		return this->tongdoanhthu * this->mucthuong / 100 + this -> base_salary *0.1;
+	}
 
 
 NhanVienHHDB::~NhanVienHHDB()
